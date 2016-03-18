ec2f4620de82513e1abf0e443164103bd9faea42
Tomasz Banasiak <tomasz.banasiak@rst.com.pl>
2016-03-18 19:30:25 +0100

    Add mysql testO

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,8 +2,23 @@
 
 require '../vendor/autoload.php';
 
+Flight::register('db', 'PDO', array('mysql:host=localhost;port=3306;dbname=resq_app', 'resq_app', 'softmasters'), function($db) {
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+});
+
 Flight::route('/', function(){
     echo 'Resq Backend API';
+});
+
+Flight::route('/mysqltest', function() {
+
+    echo "<h1>Database test</h1>";
+    $conn = Flight::db();
+    $data = $conn->query("SELECT * FROM users");
+
+    foreach($data as $row) {
+        print_r(json_encode($row));
+    }
 });
 
 Flight::start();
