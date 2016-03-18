1567e06ad13c373621fdbb10d0e21f672d5cb94e
Kacper Kozak <dexted.xt@gmail.com>
2016-03-18 19:51:37 +0100

    Create frontend/ dir

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,10 +9,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    // presets: ['es2015', 'react']
-                }
+                loader: 'babel'
             }
         ]
     },
