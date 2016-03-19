472ca15e892c758810a17824a0227de3a2a5290d
Sebastian Rosik <sebastian.rosik@rst.com.pl>
2016-03-19 07:59:31 +0100

    Fixed preloader

--- frontend/src/containers/phoneContacts/phoneContacts.jsx
+++ frontend/src/containers/phoneContacts/phoneContacts.jsx
@@ -61,7 +61,7 @@ export default class Contacts extends React.Component {
         let preloader;
 
         if (this.state.request) {
-            prealoder = <div className="staticPreloader">
+            preloader = <div className="staticPreloader">
                 <span/>
                 <span/>
                 <span/>
