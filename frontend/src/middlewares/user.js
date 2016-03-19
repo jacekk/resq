5bb9c386a4087cd41d85577895fef06885822ca6
jacekk <github.com@jacek-k.net>
2016-03-19 16:03:48 +0100

    updated login and register callbacks;

--- frontend/src/middlewares/user.js
+++ frontend/src/middlewares/user.js
@@ -3,38 +3,6 @@ import * as C from '../lib/constants';
 import {notify, hideNotification} from '../lib/notification/actions';
 import {userGetSuccess} from '../lib/user/actions';
 
-const createRequest = (next, state, resourceName) => {
-    return superagent
-        .post(C.API_URL + resourceName)
-        .send(state.user.get('account'))
-        .set('Accept', 'application/json')
-        .end((err, res) => {
-            if (res && res.body && res.body.message && res.body.message.session) {
-                next(userGetSuccess(res.body.message.session));
-                return;
-            }
-            if (err && err.status && err.status === 422) {
-                if ('login' === resourceName) {
-                    next(notify(C.NOTIFY_ERROR, 'Wrong login or password.'));
-                } else {
-                    next(notify(C.NOTIFY_ERROR, err.message));
-                }
-                return;
-            }
-            if ('login' === resourceName) {
-                next(notify(
-                    C.NOTIFY_ERROR,
-                    res && res.body && res.body.message || 'Unhandled situation. Let us now.'
-                ));
-                return;
-            }
-            window.location.hash = '/login';
-            setTimeout(() => {
-                next(notify(C.NOTIFY_SUCCESS, 'Registration succesful. You can login now.'));
-            }, 3e3);
-        });
-}
-
 export function userRegisterMiddleware({ getState }) {
     return (next) => {
         return (action) => {
@@ -42,7 +10,34 @@ export function userRegisterMiddleware({ getState }) {
                 return next(action);
             }
 
-            return createRequest(next, getState(), 'register');
+            const state = getState();
+
+            return superagent
+                .post(C.API_URL + 'register')
+                .send(state.user.get('account'))
+                .set('Accept', 'application/json')
+                .end((err, res) => {
+                    if (err && err.status && err.status === 422) {
+                        console.log(res, res.body, res.body.message);
+                        next(notify(
+                            C.NOTIFY_ERROR,
+                            res && res.body && res.body.message || 'Unexpected error. Please try again.'
+                        ));
+                        return;
+                    }
+                    if (err) {
+                        next(notify(C.NOTIFY_ERROR, err.message));
+                        return;
+                    }
+                    if (! res.body || ! res.body.id) {
+                        next(notify(C.NOTIFY_ERROR, 'Unecepected error. Please try again.'));
+                        return;
+                    }
+                    window.location.hash = '/';
+                    setTimeout(() => {
+                        next(notify(C.NOTIFY_SUCCESS, 'Registration succesful. You can login now.'));
+                    }, 2e3);
+                });
         };
     };
 };
@@ -54,7 +49,26 @@ export function userLoginMiddleware({ getState }) {
                 return next(action);
             }
 
-            return createRequest(next, getState(), 'login');
+            const state = getState();
+
+            return superagent
+                .post(C.API_URL + 'login')
+                .send(state.user.get('account'))
+                .set('Accept', 'application/json')
+                .end((err, res) => {
+                    if (res && res.body && res.body.message && res.body.message.session) {
+                        next(userGetSuccess(res.body.message.session));
+                        return;
+                    }
+                    if (err && err.status && err.status === 422) {
+                        next(notify(C.NOTIFY_ERROR, 'Wrong login or password.'));
+                        return;
+                    }
+                    next(notify(
+                       C.NOTIFY_ERROR,
+                       res && res.body && res.body.message || 'Unhandled situation. Let us now.'
+                    ));
+                });
         };
     };
 };
